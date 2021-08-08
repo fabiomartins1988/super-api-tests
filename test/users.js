67f956fff524f766926257d9989678fb4ab1747f
Fabio Martins <fabio.martins@locaweb.com>
2021-08-07 21:46:36 -0300

    add new GET test scenario

--- test/users.js
+++ test/users.js
@@ -5,13 +5,30 @@ const request = supertest('https://gorest.co.in/public/v1/');
 import { expect } from 'chai';
 
 const { TOKEN_API } = process.env
-const TOKEN = TOKEN_API
 
 describe('Users', () => {
-  it('GET /users', (done) => {
-    request.get(`users?access-token=${TOKEN}`).end((err, res) => {
+  it('GET /users', () => {
+    return request.get(`users?access-token=${TOKEN_API}`).then((res) => {
       expect(res.body.data).to.not.be.empty;
-      done();
+    });
+  });
+
+  it('GET /users/:id', () => {
+    return request.get(`users/87?access-token=${TOKEN_API}`).then((res) => {
+      expect(res.body.data.id).to.be.eq(87);
+    });
+  });
+
+  it('GET /users with query params', () => {
+    const url = `users?access-token=${TOKEN_API}&page=5&gender=female&status=active`;
+
+    return request.get(url).then((res) => {
+      expect(res.body.data).to.not.be.empty;
+      expect(res.body.meta.pagination.page).to.be.eq(5);
+      res.body.data.forEach((data) => {
+        expect(data.gender).to.eq('female');
+        expect(data.status).to.eq('active');
+      });
     });
   });
 });
